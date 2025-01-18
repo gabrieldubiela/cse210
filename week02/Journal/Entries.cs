712b4f29477e49a0285c5e3577605b63152135f6
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-01-18 18:11:31 -0300

    exceeding requirements

--- week02/Journal/Entries.cs
+++ week02/Journal/Entries.cs
@@ -2,8 +2,8 @@ public class Entries
 {
     public List<string> _all = new List<string>();
     
-    public void SaveEntrie(string _entrie, string _date, string _prompt)
+    public void SaveEntrie(string _entrie, string _date, string _prompt, _mood)
     {
-        _all.Add("Date: " + _date + " - Prompt: " + _prompt + "\n" + _entrie + "\n");
+        _all.Add("Date: " + _date + " - Prompt: " + _prompt + "\n" + _entrie + "\n" + "Mood of the day: " + _mood);
     }
 }
