f15c98a7ca05ef56050af7683102670720f8efd7
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-01-21 22:37:58 -0300

    Update fraction.cs

--- week03/Fractions/fraction.cs
+++ week03/Fractions/fraction.cs
@@ -1,1 +1,35 @@
+using System;
 
+public class Fraction (int 1, int 1)
+{
+  private int _top = 1;
+  private int _bottom = 1;
+
+  public Fraction()
+  {
+    _top = 1;
+    _bottom = 1;
+  }
+
+  public Fraction(int wholeNumber)
+  {
+    _top = wholeNumber;
+    _bottom = 1;
+  }
+  
+  public Fraction(int top, int bottom)
+  {
+    _top = top;
+    _bottom = bottom;
+  }
+  
+  public string GetFractionString()
+  {
+    Console.WriteLine($"{top}/{bottom}");
+  }
+
+  public double GetDecimalValue()
+    {
+      return (double)_top / (double)_bottom;
+    }
+}
