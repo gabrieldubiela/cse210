77a15e25ca4c51d1b744cca60b353ae810f72c9c
gabrieldubiela <gabriel.dubiela@hotmail.com>
2025-02-19 00:21:34 -0300

    w6 learning

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("Yellow", 5));
+        shapes.Add(new Rectangle("Blue", 5, 10));
+        shapes.Add(new Square("Red", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
